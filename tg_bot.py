30e38725bf5c5069a977deda1bb3f866651e0e7d
ArtemZinukov <zinykov_a@mail.ru>
2024-10-11 20:04:54 +0300

    Доделал quiz для тг

--- tg_bot.py
+++ tg_bot.py
@@ -1,31 +1,79 @@
 import logging
+import random
+import time
 
+import redis
+import telegram
 from environs import Env
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+from scripts import parse_questions, open_file
+from tg_logger import TelegramLogsHandler
 
 logger = logging.getLogger(__name__)
 
+QUESTION, TYPING_REPLY, ANSWER = range(3)
 
-def start(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+keyboard = [
+    ['Новый вопрос', 'Сдаться'],
+    ['Мой счёт']
+]
+
+reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+def start(update, context):
+    update.message.reply_text('Привет! Я бот для викторин', reply_markup=reply_markup)
+    return QUESTION
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Help!')
+def handle_new_question_request(update, context):
+    file_contents = open_file("questions/1vs1298.txt")
+    questions_and_answers = parse_questions(file_contents)
+    print(questions_and_answers)
+    question, answer = random.choice(list(questions_and_answers.items()))
+    redis_connection = context.bot_data['redis_connection']
+
+    user_id = update.message.from_user.id
+
+    redis_connection.set(f'question:{user_id}', question)
+    redis_connection.set(f'answer:{user_id}', questions_and_answers[question])
+
+    update.message.reply_text(question)
+    return ANSWER
+
+def handle_solution_attempt(update, context):
+    user_id = update.message.from_user.id
+    user_answer = update.message.text.strip().lower()
+    redis_connection = context.bot_data['redis_connection']
+    last_question = redis_connection.get(f'question:{user_id}')
+    print(last_question)
+    correct_answer = redis_connection.get(f'answer:{user_id}')
+    print(correct_answer)
+
+    if correct_answer:
+        correct_answer = correct_answer.split('.')[0].strip().lower()
+        correct_answer = correct_answer.split('(')[0].strip().lower()
+
+    if last_question and correct_answer and user_answer == correct_answer.lower():
+        update.message.reply_text("Поздравляю! Ваш ответ правильный! Нажмите 'Новый вопрос'", reply_markup=reply_markup)
+        return QUESTION
+    else:
+        update.message.reply_text("К сожалению, ваш ответ неверный. Попробуйте снова!", reply_markup=reply_markup)
+        return ANSWER
+
+def handle_surrender(update, context):
+    user_id = update.message.from_user.id
+    redis_connection = context.bot_data['redis_connection']
+    correct_answer = redis_connection.get(f'answer:{user_id}')
+    update.message.reply_text(f"Вот правильный ответ: {correct_answer}", reply_markup=reply_markup)
+    handle_new_question_request(update, context)
+    return QUESTION
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def done(update, context):
+    update.message.reply_text("До свидания! Буду рад, если вы вернетесь поиграть снова.")
+    return ConversationHandler.END
 
 
 def main() -> None:
@@ -33,18 +81,59 @@ def main() -> None:
     env.read_env()
 
     tg_bot = env.str("TG_BOT_TOKEN")
-    updater = Updater(tg_bot)
+    tg_bot_logger = env.str("TG_BOT_LOGGER_TOKEN")
+    unique_session_id = env.str('TG_CHAT_ID')
+    redis_port = env.str("REDIS_PORT")
+    redis_password = env.str("REDIS_PASSWORD")
+    redis_host = env.str("REDIS_HOST")
 
-    dispatcher = updater.dispatcher
+    tg_bot_logger = telegram.Bot(token=tg_bot_logger)
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    telegram_handler = TelegramLogsHandler(tg_bot_logger, unique_session_id)
+    telegram_handler.setLevel(logging.INFO)
+    telegram_formatter = logging.Formatter('%(message)s')
+    telegram_handler.setFormatter(telegram_formatter)
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    logger.addHandler(telegram_handler)
+    logger.setLevel(logging.INFO)
+    logger.info('Запуск бота')
 
-    updater.start_polling()
-    updater.idle()
+    while True:
+        try:
+            updater = Updater(tg_bot)
+            redis_connection = redis.Redis(host=redis_host, port=redis_port,
+                                           password=redis_password, db=0, decode_responses=True)
 
+            dispatcher = updater.dispatcher
+
+            dispatcher.bot_data['redis_connection'] = redis_connection
+
+            conv_handler = ConversationHandler(
+                entry_points=[CommandHandler('start', start)],
+
+                states={
+                    QUESTION: [MessageHandler(Filters.regex('^Новый вопрос$'), handle_new_question_request)],
+
+                    ANSWER: [MessageHandler(Filters.text & ~Filters.command & ~Filters.regex('^Сдаться'),
+                                            handle_solution_attempt,
+                                            pass_user_data=True),
+                             MessageHandler(Filters.regex('^Сдаться'), handle_surrender)
+                             ],
+
+                },
+
+                fallbacks=[CommandHandler("done", done)]
+            )
+
+            dispatcher.add_handler(conv_handler)
+
+
+            updater.start_polling()
+            updater.idle()
+
+        except Exception:
+            logger.exception('Телеграм-бот упал с ошибкой:')
+            time.sleep(5)
 
 if __name__ == '__main__':
     main()
